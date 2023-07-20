5f82329d7b9ec6e2ae3d74c30210d0c7704e9239
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-07-20 11:05:42 +0300

    initialise swiper

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,27 @@
-console.log('JS file works')
+const swiperPartners = new Swiper('.swiper-partners', {
+	// Optional parameters
+	loop: true,
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination-partners',
+	},
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next-partners',
+		prevEl: '.swiper-button-prev-partners',
+	},
+})
+
+const swiperTeam = new Swiper('.swiper-team', {
+	// Optional parameters
+	loop: true,
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination-team',
+	},
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next-team',
+		prevEl: '.swiper-button-prev-team',
+	},
+})
