eb186447d99315963427fa064368234c1b90e06b
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-07-21 21:38:07 +0300

    add media query and init wow.js

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,21 @@
+// Init wow
+let wow = new WOW(
+	{
+		boxClass:     'wow',
+		animateClass: 'animated', 
+		offset:       0,
+		mobile:       true,
+		live:         true,
+		callback:     function(box) {
+		 // The callback is fired every time an animation is started
+		 // The argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null,
+	}
+)
+wow.init()
+
+// Init swiper
 const swiperPartners = new Swiper('.swiper-partners', {
 	// Optional parameters
 	loop: true,
@@ -25,3 +43,8 @@ const swiperTeam = new Swiper('.swiper-team', {
 		prevEl: '.swiper-button-prev-team',
 	},
 })
+
+// Media queries
+if (document.documentElement.clientWidth <= 640) {
+	document.querySelector('.btn-services-js').innerHTML = 'Связаться с нами'
+}
